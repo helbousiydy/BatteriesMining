2d92c24ba8e579c1c93a8e2cc4620edfbd938f39
MarcDuquesnoy <marc.duquesnoy@u-picardie.fr>
2020-03-06 14:47:43 +0100

    initialization

--- build/lib/KeyWordsExtractions/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "0.0.8"
-
-# from KeyWordsExtractions.Current_Density import *
-from KeyWordsExtractions.Extracting_Informations import Converting_From_PDF_OR_XML_To_TXT,\
-    Filtring_And_Checking_Keywords
-# from KeyWordsExtractions.Filtring_Functions import *
